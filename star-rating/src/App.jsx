0aaa9525d849b102c47a1217d195abb315ee94e8
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2026-06-06 14:19:58 +0530

    star rating

---
+++ star-rating/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import StarRating from './components/StarRating'
+
+function App() {
+  
+  return (
+    <div className='App'>
+    <h1>Star Rating</h1>
+    <StarRating starCount={12}/>
+    </div>
+  )
+}
+
+export default App
---
+++ star-rating/src/components/StarRating.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function StarRating({ starCount }) {
+  const [starValue, setStarValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(0);
+
+  console.log(starValue);
+
+  return (
+    <div className="container">
+      {new Array(starCount).fill(0).map((_, index) => {
+        return (
+          <span
+            key={index}
+            className={
+              (hoverValue === 0 && index < starValue) || index < hoverValue
+                ? "gold"
+                : ""
+            }
+            onClick={() => setStarValue(index + 1)}
+            onMouseEnter={() => setHoverValue(index + 1)}
+            onMouseLeave={() => setHoverValue(0)}
+          >
+            &#9733;
+          </span>
+        );
+      })}
+    </div>
+  );
+}
